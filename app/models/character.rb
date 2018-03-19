3a08374ecece29957c3b09890dc77b8c7ad37830
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-19 21:00:28 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,13 +2,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
 
-  def catchphrase=(val)
-    @catchphrase = val
-  end
-
-  def catchphrase
-    @catchphrase
-  end
 
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
