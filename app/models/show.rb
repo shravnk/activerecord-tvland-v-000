f0c1111b880db4a131ab429d91aeeebe7e83a910
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-19 21:10:49 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :artists, through: :characters
+  belongs_to :network
 
   def build_network(network)
     new_network = Network.create(network)
