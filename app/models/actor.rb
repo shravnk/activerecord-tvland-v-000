d3c8aaccda0364c50a74e7bf8011a4721850ecec
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-19 20:45:35 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    "#{:first_name} #{:last_name}"
+    "#{self[:first_name]} #{self[:last_name]}"
   end
 end
