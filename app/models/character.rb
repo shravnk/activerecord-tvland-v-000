e7326826ca38c92e478d8fc072074e21d76f1299
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-19 20:50:28 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,9 @@
 class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
+
+  attr_accessor :catchphrase
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
